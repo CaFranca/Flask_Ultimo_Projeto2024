68eb1657582f396bffaa148714a6dea7d0e3c2d8
Caíque <135059792+CaFranca@users.noreply.github.com>
2024-12-13 11:45:56 -0300

    Correcoes na logica de livros,categorias e autores
    Fazer com que ao adicionar alguma categoria ou autor, mantenha-se na pagina inicial

---
+++ Ultimo_Projeto2024-Codigos_feito_na_sala/Ultimo_Projeto2024-main/controller/__init__.py
@@ -0,0 +1,6 @@
+# Importa todos os controladores necessários para a aplicação
+from .AutorController import *  # Controlador relacionado aos autores
+from .LivroController import *    # Controlador relacionado aos livros
+from .CategoriasController import *  # Controlador relacionado às categorias
+from .UsuarioController import *
+
