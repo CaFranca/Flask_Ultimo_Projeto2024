ee9008225e4041f11bea70af5c3cf09ce52be22e
Bruno <129236021+Bruno-T-S@users.noreply.github.com>
2024-12-15 21:56:10 -0300

    Detalhes e comentários

--- static/js/home.js
+++ static/js/home.js
@@ -1,7 +1,9 @@
-function toggleContent(button) {
-  const targetId = button.getAttribute('data-target'); // Obtém o ID do formulário associado
-  const content = document.getElementById(targetId); // Seleciona a div específica
+function toggleContent(button) // Declara a função para mostrar/ocultar o conteúdo
+{
+  const targetId = button.getAttribute('data-target'); // Verifica se foi selecionado o grupo "formulario1" (Livro, Categoria, Autor) ou "formulario2" (Empréstimo, AdicionarUsuário+)
+  const content = document.getElementById(targetId); // Seleciona a div formulario1 ou formulario2
 
-  content.classList.toggle('show'); // Alterna a classe "show" na div correspondente
-  button.classList.toggle('active'); // Alterna a classe "active" no botão clicado
+  content.classList.toggle('show'); // Liga ou desliga o "show" (mostrar). Quando show está ligado, nada mais tem do que o mesmo estilo da div normal.
+  // Quando show está "desligado", a div é "formularios-esconder", e tem display none.
+  button.classList.toggle('active'); // Caso o botão seja apertado, ele gira 90º
 }
