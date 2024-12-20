be86e6d673c0d3bcc17f262c62a0112fb8c3e8ea
Caíque <135059792+CaFranca@users.noreply.github.com>
2024-12-19 23:46:07 -0300

    Delete Ultimo_Projeto2024-Codigos_feito_na_sala/Ultimo_Projeto2024-main directory

--- Ultimo_Projeto2024-Codigos_feito_na_sala/Ultimo_Projeto2024-main/DAO/__init__.py
+++
@@ -1,5 +0,0 @@
-from .AutorDAO import *
-from .CategoriaDao import *
-from .LivroDAO import * 
-from .EmprestimoDAO import *
-from .UsuarioDAO import *
--- Ultimo_Projeto2024-Codigos_feito_na_sala/Ultimo_Projeto2024-main/controller/__init__.py
+++
@@ -1,7 +0,0 @@
-# Importa todos os controladores necessários para a aplicação
-from .AutorController import *  # Controlador relacionado aos autores
-from .LivroController import *    # Controlador relacionado aos livros
-from .CategoriasController import *  # Controlador relacionado às categorias
-from .UsuarioController import *
-from .EmpresimoController import *
-
